edeb56fa7477f25d765832c1439562cf655cfeb4
MaryAlex <worker96@mail.ru>
2016-03-20 13:21:37 +0300

    Disease history object added

--- app/src/main/java/com/example/mary/hospital/Model/DiseaseHistory.java
+++ app/src/main/java/com/example/mary/hospital/Model/DiseaseHistory.java
@@ -2,6 +2,38 @@ package com.example.mary.hospital.Model;
 
 public class DiseaseHistory {
 
-//    public static final String DATABASE_TABLE = ""
+    public static final String DATABASE_TABLE = "disease_histories";
+    public static final String ID_COLUMN = "id";
+    public static final String TEXT_COLUMN = "text";
 
+    private Integer id;
+    private String text;
+
+    public DiseaseHistory(Integer id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public DiseaseHistory(String text) {
+        this.text = text;
+    }
+
+    public DiseaseHistory() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
