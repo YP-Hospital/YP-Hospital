5df75a9a9d2b2e3cf83fba1f6adf1846eddbd895
MaryAlex <worker96@mail.ru>
2016-03-25 12:01:17 +0300

    Menu exist, but doesn't work

--- app/src/main/java/com/example/mary/hospital/Action/ListOfUsersActivity.java
+++ app/src/main/java/com/example/mary/hospital/Action/ListOfUsersActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -43,6 +45,29 @@ public class ListOfUsersActivity extends AppCompatActivity {
         createAndRepaintListView(0);
         createSpinner();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.certificatesPage:
+                //TODO
+                return true;
+            case R.id.mainPage:
+                //TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     public static void addUserToDoctor(int position){
         int temp = Integer.parseInt(doctorID);
--- app/src/main/java/com/example/mary/hospital/Action/UserActivity.java
+++ app/src/main/java/com/example/mary/hospital/Action/UserActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -89,5 +91,27 @@ public class UserActivity extends AppCompatActivity {
         startActivity(IntentTemp);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.certificatesPage:
+                //TODO
+                return true;
+            case R.id.mainPage:
+                //TODO
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
 
